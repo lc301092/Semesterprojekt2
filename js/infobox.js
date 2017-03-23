458eccfa1ce9f0be3613e21883a069d71b0ff8e8
Nikolai Damm <nikolai.damm@gmail.com>
2017-03-23 13:39:58 +0100

    Editable text on/off

--- js/infobox.js
+++ js/infobox.js
@@ -1,2 +1,33 @@
-document.getElementByClass("newsheader0").readOnly = true;
-document.getElementByClass("newstext0").readOnly = true;
+function editNews0(button) {
+    var x = document.getElementsByClassName("news0");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        if (x[i].contentEditable == "true") {
+            x[i].contentEditable = "false";
+        } else {
+            x[i].contentEditable = "true";
+        }
+    }
+    if (button.innerHTML == "...") {
+        button.innerHTML = "Save";
+    } else {
+        button.innerHTML = "...";
+    }
+}
+
+function editNews1(button) {
+    var x = document.getElementsByClassName("news1");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        if (x[i].contentEditable == "true") {
+            x[i].contentEditable = "false";
+        } else {
+            x[i].contentEditable = "true";
+        }
+    }
+    if (button.innerHTML == "...") {
+        button.innerHTML = "Save";
+    } else {
+        button.innerHTML = "...";
+    }
+}
