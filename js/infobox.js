7532eb5b928472fd0690b67be87f95e01f34931d
Nikolai Damm <nikolai.damm@gmail.com>
2017-03-24 12:22:36 +0100

    Added "-" button to delete news, and simplified code
    "-" button works, but simplyfiyng the code broke the editable text function. Im fixing that later.
    "+" button doesnt work yet

--- js/infobox.js
+++ js/infobox.js
@@ -1,24 +1,12 @@
-//Funktion der sørger for redigering af tekst i nyhed 0.
-function editNews0(button) {
-    var x = document.getElementsByClassName("news0");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        if (x[i].contentEditable == "true") {
-            x[i].contentEditable = "false";
-        } else {
-            x[i].contentEditable = "true";
-        }
-    }
-    if (button.innerHTML == "...") {
-        button.innerHTML = "Save";
-    } else {
-        button.innerHTML = "...";
-    }
-}
-
-//Funktion der sørger for redigering af tekst i nyhed 1.
-function editNews1(button) {
-    var x = document.getElementsByClassName("news1");
+//Sletter nyheden, ved at fjerne div tagget den er i.
+$(document).ready(function () {
+    $(".deleteNews").click(function () {
+        $(this).parent('div').remove();
+    });
+});
+//Funktion der sørger for redigering af tekst i nyheder.
+function editNews(button, news) {
+    var x = document.getElementsByClassName(news); //Virker ikke
     var i;
     for (i = 0; i < x.length; i++) {
         if (x[i].contentEditable == "true") {
