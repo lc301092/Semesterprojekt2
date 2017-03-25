be8451a6a27872f793bbbe19f9fac8df4c83a139
Nikolai Damm <nikolai.damm@gmail.com>
2017-03-25 13:26:15 +0100

    Task finished
    - All functtions works now. One can create news, edit news, and delete news. (The newest news is on the top)
    - Changed all functions to jQuery, as it was to complicated to make things works as intended with javaScript (would     probably make the code way longer anyways)
    - Reaarranged buttons

--- js/infobox.js
+++ js/infobox.js
@@ -1,29 +1,29 @@
-//Sletter nyheden, ved at fjerne div tagget den er i.
-$(document).ready(function () {
-    $(".deleteNews").click(function () {
-        $(this).parent('div').remove();
-    });
-});
-//Funktion der sørger for redigering af tekst i nyheder.
-function editNews(button, news) {
-    var thisNews;
-    if (news == news0) {
-        thisNews = "news0";
-    } else if (news == news1) {
-        thisNews = "news1"
-    }
-    var x = document.getElementsByClassName(thisNews); //Virker ikke
-    var i;
-    for (i = 0; i < x.length; i++) {
-        if (x[i].contentEditable == "true") {
-            x[i].contentEditable = "false";
-        } else {
-            x[i].contentEditable = "true";
-        }
-    }
-    if (button.innerHTML == "...") {
-        button.innerHTML = "Save";
-    } else {
-        button.innerHTML = "...";
-    }
+//Tilføjer en nyhed
+function createNews() {
+    var div = $("<div></div>").addClass("newsTab");
+    var deleteButton = $("<button>-</button>").addClass("deleteNews");
+    var editButton = $("<button>...</button>").addClass("editNews");
+    var overskrift = $("<h1>Overskrift</h1>").addClass("news").attr('contenteditable', 'false');
+    var paragraf = $("<p>Tekst...</p>").addClass("news").attr('contenteditable', 'false');
+    $(".infobox").prepend(div);
+    $(".newsTab").first().append(deleteButton, editButton, overskrift, paragraf);
 }
+//Funktion der sørger for redigering af tekst i nyheder.
+$(document).on('click', '.editNews', function () {
+    var x = $(this).parent('div').find('.news');
+    if (x.attr('contentEditable') == "true") {
+        x.attr('contentEditable', 'false');
+    } else {
+        x.attr('contentEditable', 'true');
+    }
+    if (this.innerHTML == "...") {
+        this.innerHTML = "Save";
+    } else {
+        this.innerHTML = "...";
+    }
+});
+
+//Sletter nyheden, ved at fjerne div tagget den er i.
+$(document).on('click', '.deleteNews', function () {
+    $(this).parent('div').remove();
+});
