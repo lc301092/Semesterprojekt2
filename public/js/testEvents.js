f706f5babe4822a2d5f184f9017687bac44519d6
Luca Casa <lc301092@gmail.com>
2017-05-10 12:17:54 +0200

    Farvekode
    test d. 10 maj

--- public/js/testEvents.js
+++ public/js/testEvents.js
@@ -1,9 +1,21 @@
-function testCalendar() {
+function addAndRenderBooking(username, date, color) {
 	console.log("is testCalendar wokring?");
-	var testEvent = {
-		title: 'event1',
-		start: '2017-05-05'
+	var event = {
+		title: username,
+		start: date,
+		color: color
 	}
-	$('#calendar').fullCalendar('addEvent', testEvent);
-	$('#calendar').fullCalendar('renderEvent', testEvent, true);
+	$('#calendar').fullCalendar('addEvent', event);
+	$('#calendar').fullCalendar('renderEvent', event, true);
+}
+
+function initUnavailableDates() {
+	// blocked
+	addAndRenderBooking('blokeret', '2017-05-13', 'black');
+	addAndRenderBooking('blokeret', '2017-05-14', 'black');
+	addAndRenderBooking('blokeret', '2017-05-15', 'black');
+	// resereved 
+	addAndRenderBooking('reserveret', '2017-05-27', 'red');
+	addAndRenderBooking('reserveret', '2017-05-06', 'red');
+	addAndRenderBooking('reserveret', '2017-05-17', 'red');
 }
