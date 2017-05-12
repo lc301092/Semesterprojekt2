56e3524d5deed34b674c68d52b1415a6d2a0cfdd
Luca Casa <lc301092@gmail.com>
2017-05-12 14:20:24 +0200

    WIP  - Calendar POST til database

--- public/js/testEvents.js
+++ public/js/testEvents.js
@@ -1,29 +1,61 @@
 function addAndRenderBooking(username, date, color) {
 	console.log("is testCalendar wokring?");
 	var event = {
-		title: username,
-		start: date,
-		color: color
-	}
+			title: username,
+			start: date,
+			color: color
+		}
+		//insert event
 	$('#calendar').fullCalendar('addEvent', event);
+	//render calendar
 	$('#calendar').fullCalendar('renderEvent', event, true);
 }
 
+//
+//function addAndRenderBooking(username, date, color) {
+//	console.log("is testCalendar wokring?");
+//	var event = {
+//		title: users[i].username,
+//		start: users[i].bookings[j],
+//		color: color
+//	}
+//	$('#calendar').fullCalendar('addEvent', event);
+//	$('#calendar').fullCalendar('renderEvent', event, true);
+//}
+
 function initUnavailableDates() {
-	// blocked
-	addAndRenderBooking('blokeret', '2017-05-13', 'black');
-	addAndRenderBooking('blokeret', '2017-05-14', 'black');
-	addAndRenderBooking('blokeret', '2017-05-15', 'black');
+	console.log('initUnavailableDates');
 
-	addAndRenderBooking('blokeret', '2017-06-13', 'black');
-	addAndRenderBooking('blokeret', '2017-07-14', 'black');
-	addAndRenderBooking('blokeret', '2017-07-15', 'black');
-	// resereved 
-	addAndRenderBooking('reserveret', '2017-05-27', 'red');
-	addAndRenderBooking('reserveret', '2017-06-01', 'red');
-	addAndRenderBooking('reserveret', '2017-05-17', 'red');
+	$.ajax({
+			url: '/calendarbooking',
+			method: 'POST',
+			data: {
+				userName: 'aaa'
+			}
+		})
+		.done(function (dataStr) {
+			var data = JSON.parse(dataStr);
+			console.log('from server: ', data);
 
-	addAndRenderBooking('reserveret', '2017-07-27', 'red');
-	addAndRenderBooking('reserveret', '2017-07-06', 'red');
-	addAndRenderBooking('reserveret', '2017-07-17', 'red');
+			for (var i = 0; i < data.length; i++) {
+				addAndRenderBooking(data[i].status, data[i].date,
+					'black');
+			}
+		});
+	//	// blocked
+	//	addAndRenderBooking('blokeret', '2017-05-13', 'black');
+	//	addAndRenderBooking('blokeret', '2017-05-14', 'black');
+	//	addAndRenderBooking('blokeret', '2017-05-15', 'black');
+	//
+	//	addAndRenderBooking('blokeret', '2017-06-13', 'black');
+	//	addAndRenderBooking('blokeret', '2017-07-14', 'black');
+	//	addAndRenderBooking('blokeret', '2017-07-15', 'black');
+	//	// resereved 
+	//	addAndRenderBooking('reserveret', '2017-05-27', 'red');
+	//	addAndRenderBooking('reserveret', '2017-06-01', 'red');
+	//	addAndRenderBooking('reserveret', '2017-05-17', 'red');
+	//
+	//	addAndRenderBooking('reserveret', '2017-07-27', 'red');
+	//	addAndRenderBooking('reserveret', '2017-07-06', 'red');
+	//	addAndRenderBooking('reserveret', '2017-07-17', 'red');
 }
--- routes/index.js
+++ routes/index.js
@@ -3,130 +3,193 @@ var router = express.Router();
 
 /* main page. */
 router.route('/main')
-    /* GET route for main page. */
-    .get(function (req, res) {
-        // Require our DB variable
-        var db = req.db;
-        // Get the newscollection and make it accesible in main through ejs
-        db.get('newscollection').find({}).then((data) => {
-            //Load main page
-            res.render('main', {
-                "newslist": data[0].news,
-                title: 'mainpage'
-            });
-        });
-    })
-    /* POST route for main page. */
-    .post(function (req, res) {
-        // Require our DB variable
-        var db = req.db;
-        //Define title, date and paragraph from inputs on main page
-        var title = req.body.newTitle;
-        var dato = req.body.newDato;
-        var paragraph = req.body.newParagraph;
+	/* GET route for main page. */
+	.get(function (req, res) {
+		// Require our DB variable
+		var db = req.db;
+		// Get the newscollection and make it accesible in main through ejs
+		db.get('newscollection').find({}).then((data) => {
+			//Load main page
+			res.render('main', {
+				"newslist": data[0].news,
+				title: 'mainpage'
+			});
+		});
+	})
+	/* POST route for main page. */
+	.post(function (req, res) {
+		// Require our DB variable
+		var db = req.db;
+		//Define title, date and paragraph from inputs on main page
+		var title = req.body.newTitle;
+		var dato = req.body.newDato;
+		var paragraph = req.body.newParagraph;
 
-        // Push data to newscollection
-        db.get('newscollection').update({}, {
-                "$push": {
-                    "news": {
-                        "title": title,
-                        "paragraph": paragraph,
-                        "dato": dato,
-                    }
-                }
-            },
-            function (err, doc) {
-                if (err) {
-                    // If it failed, return error
-                    res.send("There was a problem adding the information to the database.");
-                } else {
-                    // And forward to success page
-                    res.redirect("main");
-                }
-            });
-    })
-    /* DELETE route for main page. */
-    .delete(function (req, res, next) {
-        console.log("delete function works");
-        console.log(req.body);
-        var index = req.body.index;
+		// Push data to newscollection
+		db.get('newscollection').update({}, {
+				"$push": {
+					"news": {
+						"title": title,
+						"paragraph": paragraph,
+						"dato": dato,
+					}
+				}
+			},
+			function (err, doc) {
+				if (err) {
+					// If it failed, return error
+					res.send("There was a problem adding the information to the database.");
+				} else {
+					// And forward to success page
+					res.redirect("main");
+				}
+			});
+	})
+	/* DELETE route for main page. */
+	.delete(function (req, res, next) {
+		console.log("delete function works");
+		console.log(req.body);
+		var index = req.body.index;
 
-        // Require our DB variable
-        var db = req.db;
+		// Require our DB variable
+		var db = req.db;
 
-        db.get('newscollection').find({}).then((data) => {
-            console.log('[ *** ');
-            console.log(data[0].news);
-            var i = data[0].news.length - index;
-            console.log(i, data[0].news[i]);
-            console.log(' *** ]');
+		db.get('newscollection').find({}).then((data) => {
+			console.log('[ *** ');
+			console.log(data[0].news);
+			var i = data[0].news.length - index;
+			console.log(i, data[0].news[i]);
+			console.log(' *** ]');
 
-            data[0].news.splice(i, 1);
-            console.log('new array', data[0].news);
-            // delete data to newscollection
-            db.get('newscollection').update({}, {
-                    "$set": {
-                        "news": data[0].news
-                    }
-                },
-                function (err, doc) {
-                    if (err) {
-                        // If it failed, return error
-                        res.send("There was a problem deleting the object in the database.");
-                    } else {
-                        // And respond with complete
-                        res.send("Collection updated");
-                    }
-                });
-        });
-    });
-
+			data[0].news.splice(i, 1);
+			console.log('new array', data[0].news);
+			// delete data to newscollection
+			db.get('newscollection').update({}, {
+					"$set": {
+						"news": data[0].news
+					}
+				},
+				function (err, doc) {
+					if (err) {
+						// If it failed, return error
+						res.send("There was a problem deleting the object in the database.");
+					} else {
+						// And respond with complete
+						res.send("Collection updated");
+					}
+				});
+		});
+	});
+//route to edit news in newsbox
 router.route('/editnews')
-    /* POST route for main page. */
-    .post(function (req, res) {
-        //Define title, date and paragraph from inputs on main page
-        var index = req.body.index;
-        var title = req.body.title;
-        var paragraph = req.body.paragraph;
-        // Request our DB variable
-        var db = req.db;
-        db.get('newscollection').find({}).then((data) => {
-            console.log('[ *** ');
-            console.log(data[0].news);
-            var i = data[0].news.length - index;
-            console.log(i, data[0].news[i]);
-            console.log(' *** ]');
+	/* POST route for main page. */
+	.post(function (req, res) {
+		//Define title, date and paragraph from inputs on main page
+		var index = req.body.index;
+		var title = req.body.title;
+		var paragraph = req.body.paragraph;
+		// Request our DB variable
+		var db = req.db;
+		db.get('newscollection').find({}).then((data) => {
+			console.log('[ *** ');
+			console.log(data[0].news);
+			var i = data[0].news.length - index;
+			console.log(i, data[0].news[i]);
+			console.log(' *** ]');
 
-            data[0].news[i].title = title;
-            data[0].news[i].paragraph = paragraph;
+			data[0].news[i].title = title;
+			data[0].news[i].paragraph = paragraph;
 
-            console.log(i, data[0].news[i]);
+			console.log(i, data[0].news[i]);
 
-            db.get('newscollection').update({}, {
-                    $set: {
-                        "news": data[0].news
-                    }
-                }),
-                function (err, doc) {
-                    if (err) {
-                        // If it failed, return error
-                        res.send("There was a problem adding the information to the database.");
-                    } else {
-                        // And forward to success page
-                        res.send("editNews complete");
-                    }
-                }
-        });
-    });
+			db.get('newscollection').update({}, {
+					$set: {
+						"news": data[0].news
+					}
+				}),
+				function (err, doc) {
+					if (err) {
+						// If it failed, return error
+						res.send("There was a problem adding the information to the database.");
+					} else {
+						// And forward to success page
+						res.send("editNews complete");
+					}
+				}
+		});
+	});
+
+//route to INIT/GET bookings in calendar 
+router.route('/calendarbooking')
+	//GET route for calendar
+	.post(function (req, res) {
+		console.log('request ', req.body);
+
+		// getcollectionfind iterate over users etc
+		var db = req.db;
+		db.get('usercollection').find({}).then((data) => {
+			var t = [];
+			for (var i = 0; i < data[0].users.length; i++) {
+				var user = data[0].users[i].username;
+
+				for (var j = 0; j < data[0].users[i].bookings.length; j++) {
+					var dates = data[0].users[i].bookings[j];
+
+					t.push({
+						status: user,
+						date: dates
+					});
+				}
+
+			}
+			//			console.log("t ", t);
+			res.send(JSON.stringify(t));
+		});
+	});
+
+// route to bookings in calendar POST
+router.route('/calendarbookingpost')
+	/* POST route for main page. */
+	.post(function (req, res) {
+		console.log('request ', req.body);
+		var date = req.body;
+		// Request our DB variable
+		var db = req.db;
+		db.get('usercollection').find({}).then((data) => {
+			console.log('[ *** ');
+			//placeholder: index for datoen bookningen skal ske
+			console.log(' *** ]');
+			//console.log(i, data[0].news[i]);
+			db.get('usercollection').update({}, {
+					//scope er med users ikke med bookings. DET SKAL FIKSES 
+					$push: {
+						"bookings": {
+							date
+						}
+					}
+				}),
+				function (err, doc) {
+					if (err) {
+						// If it failed, return error
+						res.send("There was a problem adding the information to the database.");
+					} else {
+						// And forward to success page
+						res.send("editNews complete");
+					}
+				}
+		});
+	});
+//
+
 
 
 /* Login page. */
 router.route('/')
-    /* GET route for login page. */
-    .get(function (req, res) {
-        //Load login page
-        res.render('login', {
-            title: 'login'
-        });
-    });
+	/* GET route for login page. */
+	.get(function (req, res) {
+		//Load login page
+		res.render('login', {
+			title: 'login'
+		});
+	});
 module.exports = router;
